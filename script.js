0a512ce1eae688839d0403b9dfe2c270db9d0e69
asharinovaldy <asharinovaldi6319@gmail.com>
2022-02-11 20:57:11 +0800

    revisi 2

---
+++ script.js
@@ -0,0 +1,16 @@
+// when window is loaded
+$(window).on('load', function (){
+    $('.img-header').addClass('scale-in-hor-center');
+});
+
+$(window).on('load', function (){
+    $('.carousel-inner').addClass('focus-in-expand');
+});
+
+$(window).on('load', function (){
+    $('.description').addClass('text-focus-in');
+});
+
+$(window).on('load', function (){
+    $('.btn-cta').addClass('text-focus-in');
+});
